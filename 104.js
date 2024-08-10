de8b67d6f63591d0adb8a98b3506e4e4f483e629
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-10 21:25:45 +0530

    done querySelector and querySelectorAll(slighly)

---
+++ 104.js
@@ -0,0 +1,17 @@
+// get elements using querySelector
+
+const h1Element = document.querySelector('#main-heading');
+// console.log(h1Element);
+
+const headerElement = document.querySelector('.header');
+// console.log(headerElement);
+
+const liElement = document.querySelector('.nav-item');
+console.log(liElement);
+
+const liElements = document.querySelectorAll('.nav-item');
+console.log(liElements);
+
+// rule hain 
+// ek page pe hum id ek hi de sakte hain
+// Par classes hum multiple v de skte hain
