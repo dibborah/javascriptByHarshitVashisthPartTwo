3141c8e88bcd80caaba4730c0aa88ed20efbfd85
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-10 17:30:06 +0530

    event propogation practicing

---
+++ 126.js
@@ -0,0 +1,8 @@
+const allButtons = document.querySelectorAll('.my-buttons button');
+
+allButtons.forEach((button) => {
+    button.addEventListener('click', (event) => {
+        event.currentTarget.style.background = 'yellow';
+        event.currentTarget.style.color = 'grey';
+    })
+})
