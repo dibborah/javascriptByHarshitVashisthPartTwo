41dc6cbfe75ee83b1b4eb3d832acdcf62ed0d334
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-11 11:30:08 +0530

    done getAttributes and setAttributes

---
+++ 107.js
@@ -0,0 +1,12 @@
+// get and set attributes
+
+const link = document.querySelector('a');
+console.log(link.getAttribute('href').slice(1));
+// link.setAttribute('href', 'https://codprog.com');
+// console.log(link.getAttribute('href'));
+
+// const allLinks = document.querySelectorAll('a');
+// console.log(allLinks);
+
+const inputElement = document.querySelector('.form-todo input[type=text]');
+// console.log(inputElement.getAttribute('type'));
