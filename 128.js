a6a15c6cd535acd706470168ea78298000f1bff1
dibya.borah <dibyajyotiborah469@gmail.com>
2024-08-13 12:52:22 +0530

    done event bubbling and captures(true 3rd args) => event propogation

--- 128.js
+++ 128.js
@@ -2,4 +2,43 @@
 // event bubbling
 // event capturing
 // event propogation === event bubbling + capturing
-// event delegation
+
+
+const body = document.body;
+
+const grandparent = document.querySelector('.grandparent');
+const parent = document.querySelector('.parent');
+const child = document.querySelector('.child');
+
+body.addEventListener('click', () => {
+    console.log('body is captured up when clicked');
+}, true);
+
+grandparent.addEventListener('click', () => {
+    console.log('grandparent is captured up when clicked');
+}, true);
+
+parent.addEventListener('click', () => {
+    console.log('parent is captured up when clicked');
+}, true);
+
+child.addEventListener('click', () => {
+    console.log('child is captured up when clicked');
+}, true);// 3 arguement true captures an event
+
+body.addEventListener('click', () => {
+    console.log('body is bubbled up when clicked');
+})
+
+grandparent.addEventListener('click', () => {
+    console.log('grandparent is bubbled up when clicked');
+})
+
+parent.addEventListener('click', () => {
+    console.log('parent is bubbled up when clicked');
+})
+
+child.addEventListener('click', () => {
+    console.log('child is bubbled up when clicked');
+})
+
